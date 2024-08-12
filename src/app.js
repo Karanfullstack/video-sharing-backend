4fdba224f52de1424472a62b7607f5515f0f836c
Karan Chauhan <karanchouhan211@gmail.com>
2024-08-12 10:09:31 +0200

    server connection

--- src/app.js
+++ src/app.js
@@ -14,3 +14,5 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+import { app } from './app.js';
 import { connectDB } from './database/index.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 connectDB()
+    .then(() => {
+        const PORT = process.env.PORT;
+        app.listen(PORT, () => {
+            console.log(`Servier is connected at: ${PORT}`);
+        });
+        app.on('error', (err) =>
+            console.log('Error while connecting to server !!', err)
+        );
+    })
+    .catch((err) => console.log('MongoDB connection failed !!!', err));
